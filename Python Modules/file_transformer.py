278b81aa70109a41670967b313839c06852b7467
Ju Yeon Lee <justforher12344@gmail.com>
2021-02-12 03:07:40 +0900

    initial commit

--- Python Modules/file_transformer.py
+++ Python Modules/file_transformer.py
@@ -31,6 +31,8 @@ class STT_transformer(object):
 
         self.path = json
         self.data = self._load_json()
+        self.level1 = self._extraction()[0]
+        self.info = self._extraction()[1]
 
 
     def _load_json(self):
@@ -39,7 +41,7 @@ class STT_transformer(object):
         return data
 
 
-    def _html_adapter(self, string, tag):
+    def _html_tagger(self, string, tag):
 
         assert type(string) is str, print("text input must be string.")
         assert type(string) is str, print("tag input must be string.")
@@ -47,7 +49,7 @@ class STT_transformer(object):
         ''' 
 
         tag information
-
+        --------------------------------------------------------
         1. start : we add "<strong>" at the front part of input, which contains start time and speaker information
         2. middle : we add "</strong><br>" at the front part of the input, which is usually real script.
         3. end : we add "<br><br>" at the last part of the input, which is usually real script.
@@ -70,11 +72,21 @@ class STT_transformer(object):
             print("there is no such {} tag").format(tag)
 
 
-    def _segmentation(self, data):
+    def _extraction(self):
 
         level0 = list(self.data.values())
         level1 = list(level0[2].values())
-        level2 = level1[1]
+
+        information = dict()
+        information['full_transcript'] = list(level1[0][0].values())[0]
+        information['speaker_num'] = level1[1]['speakers']
+
+        return level1, information
+
+
+    def _segmentation(self):
+
+        level1 = self.level1
         level2 = level1[1]
         level3 = level2['segments']
 
@@ -87,17 +99,57 @@ class STT_transformer(object):
             df = df.append([(l, s, e)], ignore_index=False)
         df.columns = ['speaker', 'start_time', 'end_time']
 
-        information = dict()
-        information['full_transcript'] = list(level1[0][0].values())[0]
-        information['speaker_num'] = level2['speakers']
-
-        return df, information
+        return df
 
 
     def _parsing(self, data):
-        base_df = self._segmentation()[0]
 
-        return
+        base_df = self._segmentation()
+        level1 = self.level1
+        level2 = level1[2]
+
+        level2 = level1[2]
+        df = pd.DataFrame(level2)
+
+        num_list = pd.merge(df[['start_time', 'alternatives', 'type']].fillna('P'), base_df,\
+                            on='start_time', how='outer').dropna().reset_index()['index'].values
+
+        num_list = list(num_list)
+        num_list.append(len(df))
+        script_list = []
+
+        for i in range(len(num_list) - 1):
+            start_row = num_list[i]
+            next_row = num_list[i + 1]
+            print(start_row, next_row)
+            string = ''
+
+            for script in range(start_row, next_row):
+
+                content = df.iloc[script, 2][0]['content']
+
+                if script == start_row:
+
+                    string += content
+
+                else:
+                    if df.iloc[script, -1] == 'pronunciation':
+
+                        string = string + ' ' + content
+
+                    elif df.iloc[script, -1] == 'punctuation':
+
+                        string += content
+
+            script_list.append(string)
+
+        final_df = pd.concat([base_df.reset_index().iloc[:, 1:], pd.DataFrame(script_list, columns=['text'])], axis=1)
+
+        return final_df
+
+
+    def _html_transformer(self):
+
 
 
     def to_frontend(self):
