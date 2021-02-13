93e4d048b5d71b74fcb1e3b4f954929402fc0b80
superspray <48327038+superspray@users.noreply.github.com>
2021-02-13 13:43:38 +0900

    Update mrc_extractor.py

--- Python Modules/mrc_extractor.py
+++ Python Modules/mrc_extractor.py
@@ -28,6 +28,19 @@ def get_top3_passages(query, full_context, max_seq_len=512):
 	n_text=len(context)
 	n_token_query = len(tokenizer(query)['input_ids']) + 1
 
+	i=0
+	texts=[]
+	while i<= n_text-1:
+	  index=i+1
+	  while index<=n_text-1:
+	    if len(tokenizer(" ".join(context[i:index+1]))['input_ids']) > max_seq_len-n_token_query:
+	      break;
+	    index += 1
+	  texts.append(" ".join(context[i: index]))
+
+	  i=index
+	return texts
+
 
 
 def get_topN_answers(query, texts, n=1):
