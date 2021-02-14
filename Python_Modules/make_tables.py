6e918ad245eb2ebe2567ba490cf23366e4669b85
superspray <48327038+superspray@users.noreply.github.com>
2021-02-15 00:23:21 +0900

    Update make_tables.py

--- Python_Modules/make_tables.py
+++ Python_Modules/make_tables.py
@@ -9,22 +9,41 @@ from word2number import w2n
 
 query_dict_ex={'table_top': {
 	'Participants':'who are the attendees at the meeting?',
-	'Topic':'what was the main topic of the meeting?'
+	'Topic':'what was the main topic of the meeting?',
+    'num_agendas':'How many agendas?',
+    'num_act_items_1':'How many action items for the first agenda?', # 이후 일괄 처리 --> 질문 생성 함수 만들기
+    'num_act_items_2':'How many action items for the second agenda?' # 이후 일괄 처리 --> 질문 생성 함수 만들기
 	},
- 'table_mid_1': {
-	'Discussion Topic':'what was the topic of the first agenda?',
+ 'table_main1': {
+    'Discussion Topic':'what was the topic of the first agenda?',
 	'Presenter':'who was the presenter of the first agenda?',
 	'Conclusion':'what was the conclusion of the first agenda?'
-	},
- 'table_mid_2': {
-	'Action Items':'what was the first ?',
-	'Person Responsible':'who was the presenter of the first agenda?',
-	'Deadline':'what was the deadline of the first action item?'
-	},
-'num_tables':{
-	'num_agendas':'How many agendas?',
-	'num_act_items':'How many action items for the first agenda?'
-	}
+ },
+  'table_main1_1':{
+	'Action Items':'what was the first action item for the first agenda ?',
+	'Person Responsible':'who was responsible for the first action item  of the first agenda?',
+	'Deadline':'what was the deadline of the first action item of the first agenda?'
+  },
+    'table_main1_2':{
+	'Action Items':'what was the second action item for the first agenda ?',
+	'Person Responsible':'who was responsible for the second action item  of the first agenda?',
+	'Deadline':'what was the deadline of the second action item of the first agenda?'
+  },
+ 'table_main2': {
+    'Discussion Topic':'what was the topic of the second agenda?',
+	'Presenter':'who was the presenter of the second agenda?',
+	'Conclusion':'what was the conclusion of the second agenda?'
+ },
+  'table_main2_1':{
+	'Action Items':'what was the first action item for the second agenda ?',
+	'Person Responsible':'who was responsible for the first action item  of the second agenda?',
+	'Deadline':'what was the deadline of the first action item of the second agenda?'
+  },
+    'table_main2_2':{
+	'Action Items':'what was the second action item for the second agenda ?',
+	'Person Responsible':'who was responsible for the second action item  of the second agenda?',
+	'Deadline':'what was the deadline of the second action item of the second agenda?'
+  }
 }
 
 
@@ -34,7 +53,7 @@ class make_tables:
 
 	def __init__(self, queries_dict, full_context, type='PPC'):
 		self.date = full_context[:10] 
-		self.full_context =  full_context[10:]
+		self.full_context =  full_context[22:]
 		# if type=='PPC':
 		# 	self.num_act_items, self.num_agendas = list(MRC_EXTRACTION.get_topN_answers( \
 		# 		['How many action items for the first agenda?','How many agendas?'], self.full_context).values())  #모델 학습 후 query로 몇개인지 찾기
@@ -50,13 +69,13 @@ class make_tables:
 		temp_df = pd.DataFrame(queries_dict)
 		self.answers_dict=temp_df.replace(answers_list).to_dict()
 		self.answers_dict['Date']=self.date
-
+		print(answers_list)
 		try: 
-			self.num_agendas=w2n.word_to_num(self.answers_dict['num_tables']['num_agendas'])
+			self.num_agendas=w2n.word_to_num(self.answers_dict['table_top']['num_agendas'])
 			self.num_act_items={}
-			for i in range(self.num_agendas):
+			for i in range(1, 1+self.num_agendas):
 				try: 
-					self.num_act_items[i]=w2n.word_to_num(self.answers_dict['num_tables']['num_act_items'])
+					self.num_act_items[i]=w2n.word_to_num(self.answers_dict['table_top']['num_act_items'+str(i)])
 				except:
 					self.num_act_items[i]=0
 		except:
@@ -67,27 +86,27 @@ class make_tables:
 	# 회의록 공통 질문 및 agenda 내 첫번째 table용
 	def make_table_top(self, items=['Topic','Participants', 'Date'], loc='table_top'):
 
-	  html_string = '<table class="table table-bordered">'
+		html_string = '<table class="table table-bordered">'
 
-	  for item in items:
-	    temp='<tr><td><strong>'+item+'</strong></td>'
-	    html_string+=temp
-	    if item=='Date':
-	    	answer_str = '<td>'+self.answers_dict[item]+'/td></tr>' # model 생성 후 query 내용 넣어 수정
-	    else:
-	    	answer_str = '<td>'+self.answers_dict[loc][item]+'/td></tr>' # model 생성 후 query 내용 넣어 수정
+		for item in items:
+			temp='<tr><td><strong>'+item+'</strong></td>'
+			html_string+=temp
+			if item=='Date':
+				answer_str = '<td>'+self.answers_dict[item]+'</td></tr>' 
+			else:
+				answer_str = '<td>'+self.answers_dict[loc][item]+'</td></tr>' 
 
-	    html_string+=answer_str
+			html_string+=answer_str
 
-	  html_string+='</table>'
+		html_string+='</table>'
 
-	  return html_string
+		return html_string
 
 
 	# agenda의 두번째 table용
-	def make_table_mid_2(self, n_act_items=0,items=['Action Items','Person Responsible', 'Deadline'], loc='table_mid_2'):
+	def make_table_mid(self, n_act_items=0,items=['Action Items','Person Responsible', 'Deadline'], loc='table_main1'):
 
-		if n_act_item==0:
+		if n_act_items==0:
 			return ''
 		html_string = '<table class="table addel table-bordered" id="addel"><tr>'
 
@@ -96,9 +115,9 @@ class make_tables:
 
 		html_string+='</tr><tr>'
 
-		for i in range(self.n_act_items):
+		for i in range(1,1+n_act_items):
 			for item in items:
-				answer = '<td>'+self.answers_dict[loc][item]+'</td>' # 답 나오게 수정
+				answer = '<td>'+self.answers_dict[loc+'_'+str(i)][item]+'</td>' # 답 나오게 수정
 				html_string += answer
 
 		html_string+='</tr>'
@@ -116,14 +135,19 @@ class make_tables:
 		label = ''
 		if type=='PPC':  
 			label = 'Agenda'
-		for i in range(self.num_agendas):
-			html_string += '<label><strong>'+label+' '+str(i)+'</strong></label><table class="table table-bordered">'
-			html_string += self.make_table_top(items=['Presenter', 'Discussion Topic', 'Conclusion'], loc='table_mid_1')# 이후 적절한 query로 수정
-			html_string += self.make_table_mid_2(self.num_act_items[i]) # 수정
+		for i in range(1, 1+self.num_agendas):
+			loc='table_main'+str(i)
+			html_string += '<label><strong>'+label+' '+str(i)+'</strong></label>'
+			html_string += self.make_table_top(items=['Presenter', 'Discussion Topic', 'Conclusion'], loc=loc)# 이후 적절한 query로 수정
+			html_string += self.make_table_mid(self.num_act_items[i], loc=loc) # 수정
 
 		html_string += '<table class="table table-bordered"><tr><td><strong>Additional Notes</strong></td></tr><tr><td></td></tr></table>' # 수정
 		return html_string
 
+
+
+if __name__ == "__main__":
+	main()
 
 
 
