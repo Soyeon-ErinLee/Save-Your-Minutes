a67e43028299412e6a5517cf0ee9ab49e45a10ee
superspray <48327038+superspray@users.noreply.github.com>
2021-02-15 21:16:43 +0900

    Update make_tables.py

--- Python_Modules/make_tables.py
+++ Python_Modules/make_tables.py
@@ -20,14 +20,14 @@ query_dict_ex={'table_top': {
 	'Conclusion':'what was the conclusion of the first agenda?'
  },
   'table_main1_1':{
-	'Action Items':'what was the first action item for the first agenda ?',
-	'Person Responsible':'who was responsible for the first action item  of the first agenda?',
-	'Deadline':'what was the deadline of the first action item of the first agenda?'
+	'Action Item':'what was the first task for the first agenda ?',
+	'Person Responsible':'who was responsible for the first task  of the first agenda?',
+	'Deadline':'when is the deadline of the first task of the first agenda?'
   },
     'table_main1_2':{
-	'Action Items':'what was the second action item for the first agenda ?',
-	'Person Responsible':'who was responsible for the second action item  of the first agenda?',
-	'Deadline':'what was the deadline of the second action item of the first agenda?'
+	'Action Item':'what was the second task for the first agenda ?',
+	'Person Responsible':'who was responsible for the second task  of the first agenda?',
+	'Deadline':'when is the deadline of the second task of the first agenda?'
   },
  'table_main2': {
     'Discussion Topic':'what was the topic of the second agenda?',
@@ -35,30 +35,25 @@ query_dict_ex={'table_top': {
 	'Conclusion':'what was the conclusion of the second agenda?'
  },
   'table_main2_1':{
-	'Action Items':'what was the first action item for the second agenda ?',
-	'Person Responsible':'who was responsible for the first action item  of the second agenda?',
-	'Deadline':'what was the deadline of the first action item of the second agenda?'
+	'Action Item':'what was the first task for the second agenda?',
+	'Person Responsible':'who was responsible for the first task  of the second agenda?',
+	'Deadline':'when is the deadline of the first task of the second agenda?'
   },
     'table_main2_2':{
-	'Action Items':'what was the second action item for the second agenda ?',
-	'Person Responsible':'who was responsible for the second action item  of the second agenda?',
-	'Deadline':'what was the deadline of the second action item of the second agenda?'
+	'Action Item':'what was the second task for the second agenda ?',
+	'Person Responsible':'who was responsible for the second task  of the second agenda?',
+	'Deadline':'when is the deadline of the second task of the second agenda?'
   }
 }
 
 
-## number  처리할것 
 #질문 부분 수정
 class make_tables:
 
 	def __init__(self, queries_dict, full_context, type='PPC'):
 		self.date = full_context[:10] 
 		self.full_context =  full_context[22:]
-		# if type=='PPC':
-		# 	self.num_act_items, self.num_agendas = list(MRC_EXTRACTION.get_topN_answers( \
-		# 		['How many action items for the first agenda?','How many agendas?'], self.full_context).values())  #모델 학습 후 query로 몇개인지 찾기
-
-		#질문 수정할 것
+		self.type=type
 		queries=[list(x.values()) for x in  list(queries_dict.values())]
 		queries = [item for sublist in queries for item in sublist]
 
@@ -69,12 +64,13 @@ class make_tables:
 		temp_df = pd.DataFrame(queries_dict)
 		self.answers_dict=temp_df.replace(answers_list).to_dict()
 		self.answers_dict['Date']=self.date
+		#print(answers_list)
 		try: 
 			self.num_agendas=w2n.word_to_num(self.answers_dict['table_top']['num_agendas'])
 			self.num_act_items={}
 			for i in range(1, 1+self.num_agendas):
 				try: 
-					self.num_act_items[i]=w2n.word_to_num(self.answers_dict['table_top']['num_act_items'+str(i)])
+					self.num_act_items[i]=w2n.word_to_num(self.answers_dict['table_top']['num_act_items_'+str(i)])
 				except:
 					self.num_act_items[i]=0
 		except:
@@ -103,11 +99,11 @@ class make_tables:
 
 
 	# agenda의 두번째 table용
-	def make_table_mid(self, n_act_items=0,items=['Action Items','Person Responsible', 'Deadline'], loc='table_main1'):
+	def make_table_mid(self, n_act_items=0,items=['Action Item','Person Responsible', 'Deadline'], n_agenda=1 ,loc='table_main1'):
 
 		if n_act_items==0:
 			return ''
-		html_string = '<table class="table addel table-bordered" id="addel"><tr>'
+		html_string = '<table class="table addel table-bordered" id="adde'+str(n_agenda)+'"><tr>'
 
 		for item in items:
 			html_string += '<td><strong>'+item+'</strong></td>'
@@ -116,19 +112,21 @@ class make_tables:
 
 		for i in range(1,1+n_act_items):
 			for item in items:
-				answer = '<td>'+self.answers_dict[loc+'_'+str(i)][item]+'</td>' # 답 나오게 수정
+				answer = '<td>'+self.answers_dict[loc+'_'+str(i)][item]+'</td>' 
 				html_string += answer
+			if i==n_act_items:
+				html_string+='</tr><tr>'
+			else:     
+				html_string+='</tr>'
 
-		html_string+='</tr>'
-
-		html_string+='<input type="button" class="float" value="Add" id="addrow2"></table>'
+		html_string+='<input type="button" class="float" value="Add" id="addrow'+str(n_agenda)+'"></table>'
 
 		return html_string
 
 
 	# 각 Agenda table 생성용
 
-	def make_table_final(self, type='PPC'):
+	def make_table_final(self):
 
 		html_string = self.make_table_top()
 		label = ''
@@ -137,16 +135,24 @@ class make_tables:
 		for i in range(1, 1+self.num_agendas):
 			loc='table_main'+str(i)
 			html_string += '<label><strong>'+label+' '+str(i)+'</strong></label>'
-			html_string += self.make_table_top(items=['Presenter', 'Discussion Topic', 'Conclusion'], loc=loc)# 이후 적절한 query로 수정
-			html_string += self.make_table_mid(self.num_act_items[i], loc=loc) # 수정
-
-		html_string += '<table class="table table-bordered"><tr><td><strong>Additional Notes</strong></td></tr><tr><td></td></tr></table>' # 수정
+			html_string += self.make_table_top(items=['Presenter', 'Discussion Topic', 'Conclusion'], loc=loc)
+			html_string += self.make_table_mid(self.num_act_items[i], n_agenda=i, loc=loc) 
+		html_string += '<table class="table table-bordered"><tr><td><strong>Additional Notes</strong></td></tr><tr><td></td></tr></table>' 
 		return html_string
 
+	def make_java(self):
+		java_string=''
+		for i in range(1, 1 + self.num_agendas):
+			java_string += '<script>   $(document).ready(function () {var tbody = $("#addel'+str(i)+'").children("tbody"); var table = tbody.length ? tbody : $("#addel'+str(i)+'"); $("#addrow'+str(i)+'").click(function () {table.append("<tr><td></td><td></td><td></td></tr>");})});</script>'
+		return java_string            
 
 
-if __name__ == "__main__":
-	main()
-
-
+	def get_table():
+		result_tb=self.make_table_final()
+		result_jv=self.make_java()
+		with open("Output.txt", "w") as text_file:
+			text_file.write(result_tb)
+		with open("javascript.txt", "w") as text_file:
+			text_file.write(result_jv)
+		return result_tb, result_jv
 
