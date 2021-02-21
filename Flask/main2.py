0bbc8463ba9080db6ee9c64811fa747bd0809109
Erin <65602256+Soyeon-ErinLee@users.noreply.github.com>
2021-02-21 14:20:21 +0900

    updated

--- Flask/main2.py
+++ Flask/main2.py
@@ -84,17 +84,6 @@ def upload():
 @app.route("/result", methods=['GET', 'POST'])
 def result():
     path, file_type = insert_audio()
-    if request.method == 'POST':
-        f = request.files.get('file')
-        f.save(os.path.join(upload_dir, f.filename))
-    return render_template(
-        'result.html', path=path, file_type=file_type
-    )
-
-
-@app.route("/result_interview", methods=['GET', 'POST'])
-def result1():
-    path, file_type = insert_audio()
     mtl = make_tables.MAKE_TABLES(query_faq['query_dict_agenda'], model, types="Agenda")
     mtl.get_table()
     with open(os.path.join(basedir, "templates/result.html"), "r", encoding="UTF-8") as file:
@@ -105,13 +94,30 @@ def result1():
         result_3 = file.read()
     with open(os.path.join(upload_dir, "Output.txt"), 'r', encoding="UTF-8") as file:
         txt = file.read()
+    with open(os.path.join(upload_dir, "javascript.txt"), 'r', encoding="UTF-8") as file:
+        txt2 = file.read()
     result_1 += front
     result_1 += result_2
     result_1 += txt
     result_1 += result_3
+    result_1 += txt2
+    result_1 += ("</body></html>")
     with open(os.path.join(basedir, "templates/final_result.html"), "w", encoding="UTF-8") as file:
         file.write(result_1)
     return render_template('final_result.html', path=path, file_type=file_type)
+
+
+
+@app.route("/result_interview", methods=['GET', 'POST'])
+def result1():
+    path, file_type = insert_audio()
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join(upload_dir, f.filename))
+    return render_template(
+        'result1.html', path=path, file_type=file_type
+    )
+
 
 
 @app.route("/result_idea", methods=['GET', 'POST'])
